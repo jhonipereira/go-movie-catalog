0b0b768d545bb6112b233c76d92ef481323682d9
Jhoni <113802785+jhonipereira@users.noreply.github.com>
2023-09-21 11:11:00 +0100

    Adding communication JWT structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,14 +7,20 @@ import (
 	"jhonidev/go/go-movie-catalog/internal/repository/dbrepo"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = 8080
 
 type application struct {
-	Domain string
-	DSN    string //data source name
-	DB     repository.DatabaseRepo
+	Domain       string
+	DSN          string //data source name
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
@@ -23,6 +29,11 @@ func main() {
 
 	// read from cmd
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "shhhhhh-lol", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
 	flag.Parse()
 
 	// connect to the DB
@@ -33,8 +44,18 @@ func main() {
 	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
 	defer app.DB.Connection().Close() //when the (main) func exits, it exits too
 
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
+
 	//start webserver
-	app.Domain = "example.com"
 	log.Printf("Application started at port %d\n", port)
 
 	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -8,4 +8,6 @@ import (
 type DatabaseRepo interface {
 	Connection() *sql.DB
 	AllMovies() ([]*models.Movie, error)
+	GetUserByEmail(email string) (*models.User, error)
+	GetUserByID(id int) (*models.User, error)
 }
