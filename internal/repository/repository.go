68cf1578b660f393b71494e7bb59507a8ee7e007
Jhoni <113802785+jhonipereira@users.noreply.github.com>
2023-09-22 10:39:07 +0100

    finished 1st sprint

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -7,7 +7,15 @@ import (
 
 type DatabaseRepo interface {
 	Connection() *sql.DB
-	AllMovies() ([]*models.Movie, error)
+	AllMovies(genre ...int) ([]*models.Movie, error)
 	GetUserByEmail(email string) (*models.User, error)
 	GetUserByID(id int) (*models.User, error)
+
+	OneMovieForEdit(id int) (*models.Movie, []*models.Genre, error)
+	OneMovie(id int) (*models.Movie, error)
+	AllGenres() ([]*models.Genre, error)
+	InsertMovie(movie models.Movie) (int, error)
+	UpdateMovieGenres(id int, genreIds []int) error
+	UpdateMovie(movie models.Movie) error
+	DeleteMovie(id int) error
 }
